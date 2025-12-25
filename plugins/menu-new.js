88c035a6cf936d8363f3723c227679b1e60db086
PLAYER={567}™ <player.tm.567@gmail.com>
2025-12-25 10:12:37 -0800

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
+                newsletterJid: '12036334873998720@newsletter',
                 newsletterName: config.OWNER_NAME,
                 serverMessageId: 143
             }
